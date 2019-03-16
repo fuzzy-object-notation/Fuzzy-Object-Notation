833c89bc8386d608d93fd6175ec8401f91a4e694
toonnyy8 <toonnyy8@gmail.com>
2019-03-16 18:02:56 +0800

    新增 Material Design UI

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import "@babel/polyfill"
+import "../lib/@material/material-components-web.min.js"
+import "../lib/@material/material-components-web.min.css"
 import Vue from "vue"
 import App from "../vue/app.vue"
 
